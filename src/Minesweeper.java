1e34466186943276d9dbd18e9c1516810220c294
2xyu <connerwillson86@gmail.com>
2023-05-26 07:57:18 -0400

    Update Minesweeper.java

--- src/Minesweeper.java
+++ src/Minesweeper.java
@@ -14,8 +14,8 @@ public class Minesweeper {
     static final String B8_GREY = "\u001B[38;2;123;123;123m" + "\u001B[48;2;170;170;170m";
     static final String TEXTHISTORY_DARKGREY = "\u001B[38;2;128;128;128m";
 
-    static byte height;
-    static byte width;
+    static int height;
+    static int width;
     static boolean[][] grid;
     static boolean[][] seen;
     static boolean[][] flagged;
@@ -28,75 +28,74 @@ public class Minesweeper {
 
 
     public static void displayGrid(){
-        String outP = "\t ";
-        for (byte i = 0; i < width; i++){
-            outP += ((char) ('a' + i) + " ");
+        StringBuilder outP = new StringBuilder("\t ");
+        for (int i = 0; i < width; i++){
+            outP.append((char) ('a' + i)).append(" ");
         }
-        outP += "\n\t " + "_".repeat(width * 2 - 1);
-        outP += "\n";
-        for (byte y = 0; y < height; y++){
-            outP += y + 1 + "\t|";
-            for (byte x = 0; x < width; x++){
+        outP.append("\n\t ").append("_".repeat(width * 2 - 1));
+        outP.append("\n");
+        for (int y = 0; y < height; y++){
+            outP.append(y + 1).append("\t|");
+            for (int x = 0; x < width; x++){
 
                 if (flagged[y][x]){
-                    outP += BOMB_RED + "F" + RESET + " ";
+                    outP.append(BOMB_RED + "F" + RESET + " ");
                 }
                 else if (seen[y][x]) {
 
                     boolean cell = grid[y][x];
-                    byte bombs = countBombsAround(x, y);
+                    int bombs = countBombsAround(x, y);
 
                     if (cell) {
-                        outP += BOMB_RED + "X" + RESET + " ";
+                        outP.append(BOMB_RED + "X" + RESET + " ");
                     }
                     else if (bombs == 0){
-                        outP += "  ";
+                        outP.append("  ");
                     }
-                    else if (!cell) {
+                    else {
                         if (bombs == 1){
-                            outP += B1_BLUE + bombs + RESET + " ";
+                            outP.append(B1_BLUE).append(bombs).append(RESET).append(" ");
                         }
                         else if (bombs == 2){
-                            outP += B2_GREEN + bombs + RESET + " ";
+                            outP.append(B2_GREEN).append(bombs).append(RESET).append(" ");
                         }
                         else if (bombs == 3){
-                            outP += B3_RED + bombs + RESET + " ";
+                            outP.append(B3_RED).append(bombs).append(RESET).append(" ");
                         }
                         else if (bombs == 4){
-                            outP += B4_DARKBLUE + bombs + RESET + " ";
+                            outP.append(B4_DARKBLUE).append(bombs).append(RESET).append(" ");
                         }
                         else if (bombs == 5){
-                            outP += B5_DARKRED + bombs + RESET + " ";
+                            outP.append(B5_DARKRED).append(bombs).append(RESET).append(" ");
                         }
                         else if (bombs == 6){
-                            outP += B6_TURQUOISE + bombs + RESET + " ";
+                            outP.append(B6_TURQUOISE).append(bombs).append(RESET).append(" ");
                         }
                         else if (bombs == 7){
-                            outP += B7_BLACK + bombs + RESET + " ";
+                            outP.append(B7_BLACK).append(bombs).append(RESET).append(" ");
                         }
                         else if (bombs == 8){
-                            outP += B8_GREY + bombs + RESET + " ";
+                            outP.append(B8_GREY).append(bombs).append(RESET).append(" ");
                         }
-
                     }
                 }
 
                 else {
-                    outP += "? ";
+                    outP.append("? ");
                 }
 
             }
-            outP = outP.substring(0, outP.length() - 1) + "|\n";
+            outP = new StringBuilder(outP.substring(0, outP.length() - 1) + "|\n");
         }
-        outP += "\t " + "-".repeat(width * 2 - 1);
+        outP.append("\t ").append("-".repeat(width * 2 - 1));
         System.out.println(outP);
     }
 
-    public static boolean clickGrid(byte x, byte y){
+    public static boolean clickGrid(int x, int y){
 
         if (grid[y][x]){
-            for (byte col = 0; col < width; col++){
-                for (byte row = 0; row < height; row++){
+            for (int col = 0; col < width; col++){
+                for (int row = 0; row < height; row++){
                     seen[row][col] = true;
                 }
             }
@@ -105,8 +104,8 @@ public class Minesweeper {
         else {
             seen[y][x] = true;
             if (countBombsAround(x, y) == 0) {
-                for (byte col = (byte) Math.max(0, x - 1); col < Math.min(width, x + 2); col++) {
-                    for (byte row = (byte) Math.max(0, y - 1); row < Math.min(height, y + 2); row++) {
+                for (int col = (int) Math.max(0, x - 1); col < Math.min(width, x + 2); col++) {
+                    for (int row = (int) Math.max(0, y - 1); row < Math.min(height, y + 2); row++) {
                         if (row == y && col == x) {
                             continue;
                         }
@@ -124,8 +123,8 @@ public class Minesweeper {
 
     public static boolean gameWon(){
 
-        for (byte x = 0; x < width; x++){
-            for (byte y = 0; y < height; y++){
+        for (int x = 0; x < width; x++){
+            for (int y = 0; y < height; y++){
                 if (grid[y][x] != flagged[y][x]){
                     return false;
                 }
@@ -134,11 +133,11 @@ public class Minesweeper {
         return true;
     }
 
-    public static byte countBombsAround(byte x, byte y) {
-        byte count = 0;
+    public static int countBombsAround(int x, int y) {
+        int count = 0;
 
-        for (byte col = (byte) Math.max(0, x - 1); col < Math.min(width, x + 2); col++){
-            for (byte row = (byte) Math.max(0, y - 1); row < Math.min(height, y + 2); row++){
+        for (int col = Math.max(0, x - 1); col < Math.min(width, x + 2); col++){
+            for (int row = Math.max(0, y - 1); row < Math.min(height, y + 2); row++){
                 if (row == y && col == x){
                     continue;
                 }
@@ -150,13 +149,13 @@ public class Minesweeper {
         return count;
     }
 
-    public static void flagGrid(byte x, byte y){
+    public static void flagGrid(int x, int y){
 
         flagged[y][x] = !flagged[y][x];
 
     }
 
-    public static void resizeGird(byte x, byte y){
+    public static void resizeGird(int x, int y){
         width = x;
         height = y;
         grid = new boolean[y][x];
@@ -167,19 +166,19 @@ public class Minesweeper {
 
     public static void addbomb(int b){
         bombs = b;
-        short max = (short) (width * height);
+        int max = width * height;
 
-        List<Short> temp = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
 
 
-        for (short i = 0; i < max; i++){
+        for (int i = 0; i < max; i++){
             temp.add(i);
         }
         Collections.shuffle(temp);
 
-        for (byte i = 0; i < bombs; i++){
-            byte y = (byte) (temp.get(i) % height);
-            byte x = (byte) (temp.get(i) / height);
+        for (int i = 0; i < bombs; i++){
+            int y = temp.get(i) % height;
+            int x = temp.get(i) / height;
 
 
             grid[y][x] = true;
@@ -192,8 +191,8 @@ public class Minesweeper {
         boolean lost = false;
         boolean won = false;
 
-        resizeGird((byte) 6, (byte) 6);
-        addbomb((short) 3);
+        resizeGird(6, 6);
+        addbomb(3);
         displayGrid();
 
         Scanner scanner = new Scanner(System.in);
@@ -223,8 +222,8 @@ public class Minesweeper {
                         RESET);
             }
             else if (click.matcher(action).matches()){
-                byte x = (byte) (action.split(" ")[1].split("-")[0].charAt(0) - 'a');
-                byte y = (byte) (Integer.parseInt(action.split(" ")[1].split("-")[1]) - 1);
+                int x = action.split(" ")[1].split("-")[0].charAt(0) - 'a';
+                int y = Integer.parseInt(action.split(" ")[1].split("-")[1]) - 1;
                 if (x >= width || y >= height){
                     System.out.println("Unable to click " + B3_RED + action.split(" ")[1] + RESET + " error: out of bounds to the grid.");
                     continue;
@@ -240,8 +239,8 @@ public class Minesweeper {
 
             }
             else if (flag.matcher(action).matches()){
-                byte x = (byte) (action.split(" ")[1].split("-")[0].charAt(0) - 'a');
-                byte y = (byte) (Integer.parseInt(action.split(" ")[1].split("-")[1]) - 1);
+                int x = action.split(" ")[1].split("-")[0].charAt(0) - 'a';
+                int y = Integer.parseInt(action.split(" ")[1].split("-")[1]) - 1;
 
                 if (x >= width || y >= height){
                     System.out.println("Unable to flag " + B3_RED + action.split(" ")[1] + RESET + " error: out of bounds to the grid.");
@@ -255,8 +254,8 @@ public class Minesweeper {
 
             }
             else if (resize.matcher(action).matches()){
-                byte x = (byte) Integer.parseInt(action.split(" ")[1].split("x")[0]);
-                byte y = (byte) Integer.parseInt(action.split(" ")[1].split("x")[1]);
+                int x = Integer.parseInt(action.split(" ")[1].split("x")[0]);
+                int y = Integer.parseInt(action.split(" ")[1].split("x")[1]);
 
                 if (x > 26 || y > 99){
                     System.out.println("Unable to resize to " + B3_RED + action.split(" ")[1] + RESET + " error: unit bigger than 99.");
@@ -268,7 +267,7 @@ public class Minesweeper {
                         "\t| <resized grid to " +  action.split(" ")[1] + ">" + RESET + "\n");
             }
             else if (addbomb.matcher(action).matches()){
-                int bombs = (byte) Integer.parseInt(action.split(" ")[1]);
+                int bombs = (int) Integer.parseInt(action.split(" ")[1]);
 
                 if (bombs > width * height){
                     System.out.println("Unable to add " + B3_RED + action.split(" ")[1] + RESET + " error: more bombs than the grid can fit.");
